b12c4b7068f842d51c2f6ffc28f7d04ff65bac8b
hemgaard <pehem22@student.sdu.dk>
2025-02-12 10:37:58 +0100

    bullet class

---
+++ bullet.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Bullet:
+
+    def __init__(self, x, y, speed, image_path):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.image = image_path
+        self.rect = self.image.get_rect(topleft =(self.x, self.y))
+
+        
+    
+    def draw(self, screen):
+        screen.blit(self.image,(self.x, self.y))
